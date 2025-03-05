34170b75fbf66507274177309eadf36a16cd789c
Yashpanchal3903 <95518205+Yashpanchal3903@users.noreply.github.com>
2025-03-05 18:13:23 +0530

    Create steamlit_app.py
    copied from my Custom Smoothie SiS app

---
+++ steamlit_app.py
@@ -0,0 +1,50 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, when_matched
+
+
+st.title("🥤 Customize Your Smoothie! 🥤")
+
+st.write(
+    f"""Choose the fruits you want in your custom  Smoothie!: """)
+
+
+name_on_order = st.text_input("Name of the Smoothie")
+st.write("The name of your  Smoothie will be ", name_on_order)
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+ingredients_list = st.multiselect('Choose up to 5 ingredients :',
+                                  my_dataframe,
+                                 max_selections=5)
+
+if ingredients_list:
+           
+            ingredients_string =''
+            for fruit_chosen in ingredients_list:
+                ingredients_string+=fruit_chosen + ' '
+                #st.write(ingredients_string)
+
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+                        values ('""" + ingredients_string + """,'"""+name_on_order+"""')"""
+      
+    
+            Submitted = st.button('Submit ')
+            if Submitted:
+                st.success('Someone clicked the button', icon = '👍')
+
+my_dataframe = session.table("SMOOTHIES.PUBLIC.ORDERS").filter(col("ORDER_FILLED") == False).collect()
+
+
+
+editable_df = st.data_editor(my_dataframe)
+
+
+
+            
